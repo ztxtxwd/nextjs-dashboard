ec17770bff5dca4dd7bb1ab86a40d1728a1c8330
ztxtxwd <ztx4215@gmail.com>
2024-07-11 13:43:06 +0000

    默认的email和password

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -38,6 +38,7 @@ export default function LoginForm() {
                 name="email"
                 placeholder="Enter your email address"
                 required
+                value="user@nextmail.com"
               />
               <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
@@ -58,6 +59,7 @@ export default function LoginForm() {
                 placeholder="Enter password"
                 required
                 minLength={6}
+                value="123456"
               />
               <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
