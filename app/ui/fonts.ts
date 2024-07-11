669ae421d12e1b89b044a6708fc9fb06c48f38e3
ztxtxwd <ztx4215@gmail.com>
2024-07-11 13:24:56 +0000

    initial commit

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,9 +1,5 @@
 import { Inter } from 'next/font/google';
 import { Lusitana } from 'next/font/google';
- 
 export const inter = Inter({ subsets: ['latin'] });
-
-export const lusitana = Lusitana({
-    subsets: ['latin'],
-    weight: ['400', '700'], 
-});
+// Bug 修复：添加 weight 属性
+export const lusitana = Lusitana({ weight: ['400','700'], subsets: ['latin'] });
--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { updateInvoice,State } from '@/app/lib/actions';
+import { useActionState } from'react';
 import { CustomerField, InvoiceForm } from '@/app/lib/definitions';
 import {
   CheckIcon,
@@ -17,8 +19,11 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
+  const initialState: State = { message: null, errors: {} };
+  const updateInvoiceWithId = updateInvoice.bind(null,invoice.id);
+  const [state, formAction] = useActionState(updateInvoiceWithId, initialState);
   return (
-    <form>
+    <form action={formAction}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
